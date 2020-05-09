ad27ccbc14f9adcdbe8352cd5b04f69b009c038f
XarenySC <xarenysc@gmail.com>
2020-05-09 16:52:39 -0500

    Modifies css configuration

--- pages/About.js
+++ pages/About.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import Head from 'next/head';
-import Saludo from '../Components/Saludo'
-import Navigation from '../Components/Navigation'
+import React from "react";
+import Head from "next/head";
+import Saludo from "../Components/Saludo";
+import Navigation from "../Components/Navigation";
 const About = () => {
-    return(
-        <>
-            <Head>
-                <title>About</title>
-            </Head>
-            
-            <Navigation />
-            <h1>About</h1>
-            <Saludo />
-        </>
-    )
-}
+	return (
+		<>
+			<Head>
+				<title>About</title>
+			</Head>
+
+			<Navigation />
+			<h1>About</h1>
+			<Saludo />
+		</>
+	);
+};
 
 export default About;
